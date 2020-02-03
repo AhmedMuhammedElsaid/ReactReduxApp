b203949293cf63ae7c2f0ca38beed21825ec5e41
AhmedMuhammedElsaid <ahmed.muhammed.elsaid@gmail.com>
2020-02-03 18:42:04 +0200

    Updates

---
+++ precache-manifest.ba38cd2afd40b0b1c424689354a949f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b02aa430e65849eb27456f77fb8ee0d",
+    "url": "/ReactReduxApp/index.html"
+  },
+  {
+    "revision": "26e12991d3c77eaf4cb1",
+    "url": "/ReactReduxApp/static/css/2.d4d39a4b.chunk.css"
+  },
+  {
+    "revision": "e7b9abb42b4327d99b5a",
+    "url": "/ReactReduxApp/static/css/main.08c3705e.chunk.css"
+  },
+  {
+    "revision": "26e12991d3c77eaf4cb1",
+    "url": "/ReactReduxApp/static/js/2.791c16fb.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/ReactReduxApp/static/js/2.791c16fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b9abb42b4327d99b5a",
+    "url": "/ReactReduxApp/static/js/main.9c77b4e6.chunk.js"
+  },
+  {
+    "revision": "30e1318dd0ab8c5c634a",
+    "url": "/ReactReduxApp/static/js/runtime-main.671b6dcf.js"
+  },
+  {
+    "revision": "e2003fbe4cf2e3cde72c5a1bee25c79a",
+    "url": "/ReactReduxApp/static/media/reminder.e2003fbe.png"
+  }
+]);
